8e4329d2149ab5b929fcfab8f5d1147e0b06bb3c
MBadriNarayanan <58718023+MBadriNarayanan@users.noreply.github.com>
2019-12-31 12:58:45 +0530

    Quadratic Probing Hashing Header File

---
+++ QProbing1.h
@@ -0,0 +1,10 @@
+#ifndef QProbing1_h
+#define QProbing1_h
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+void insert(int a[],int hFn, int size);
+void del(int a[],int hFn,int size);
+void search(int a[],int hFn,int size);
+void display(int a[],int size);
+#endif
